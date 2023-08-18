17359cbbbcf32557e9c15b34a5584eabfcf1bb5c
Corey Neuberger <coreyneuberger@gmail.com>
2023-08-18 16:19:04 -0400

    updated api backend links

--- client/src/page/Gallery.jsx
+++ client/src/page/Gallery.jsx
@@ -27,7 +27,7 @@ const Gallery = () => {
         setLoading(true);
     
         try {
-          const response = await fetch("https://digitaloasis-backend.up.railway.app/api/posts", {
+          const response = await fetch("https://digital-oasis-backend.vercel.app/api/posts", {
             method: 'GET',
             headers: {
               'Content-Type': 'application/json',
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
         if (form.prompt) {
             try {
                 setGeneratingImg(true);
-                const response = await fetch('https://digitaloasis-backend.up.railway.app/api/dalle2', {
+                const response = await fetch('https://digital-oasis-backend.vercel.app/api/dalle2', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -54,7 +54,7 @@ const Home = () => {
         if (form.prompt && form.image) {
             setLoading(true);
             try {
-                const response = await fetch('https://digitaloasis-backend.up.railway.app/api/posts', {
+                const response = await fetch('https://digital-oasis-backend.vercel.app/api/posts', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
