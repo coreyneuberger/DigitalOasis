4f360e5921e25717fa10fdc2a18f053fae5758ba
Corey Neuberger <coreyneuberger@gmail.com>
2023-08-18 16:57:00 -0400

    proxy

--- client/src/page/Gallery.jsx
+++ client/src/page/Gallery.jsx
@@ -27,7 +27,7 @@ const Gallery = () => {
         setLoading(true);
     
         try {
-          const response = await fetch("https://digital-oasis-backend.vercel.app/api/posts", {
+          const response = await fetch("/api/posts", {
             method: 'GET',
             headers: {
               'Content-Type': 'application/json',
