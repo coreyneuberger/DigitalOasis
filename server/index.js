b585ba94f7fc73ba0a9fc462e50505374865afb3
Corey Neuberger <coreyneuberger@gmail.com>
2023-08-18 17:34:54 -0400

    cors

--- server/index.js
+++ server/index.js
@@ -8,12 +8,35 @@ import postRoutes from './routes/postRoutes.js';
 
 dotenv.config();
 
+
+
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+  
+  const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+  }
+  
+  module.exports = allowCors(handler)
+  
+
 const app = express();
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST'],
-}
-));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/posts', postRoutes);
