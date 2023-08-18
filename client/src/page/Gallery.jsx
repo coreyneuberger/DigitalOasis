58e7f5069f1adf87cd583a8bca4c3841f511c631
Corey Neuberger <coreyneuberger@gmail.com>
2023-08-18 17:37:17 -0400

    reverted cors

--- client/src/page/Gallery.jsx
+++ client/src/page/Gallery.jsx
@@ -27,7 +27,7 @@ const Gallery = () => {
         setLoading(true);
     
         try {
-          const response = await fetch("/api/posts", {
+          const response = await fetch("https://digitaloasis-backend.up.railway.app/api/posts", {
             method: 'GET',
             headers: {
               'Content-Type': 'application/json',
